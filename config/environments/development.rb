b9a8334930cb8a6f98aa904ad95afad64bafff95
billy3321 <billy3321@gmail.com>
2016-07-29 16:37:22 +0800

    fix the development url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: Setting.url.host, protocol: Setting.url.protocol }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
   config.action_mailer.perform_deliveries = false
