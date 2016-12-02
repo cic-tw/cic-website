8fd9eb6199d22ef6be413ccf295f0e3635353ab1
billy3321 <billy3321@gmail.com>
2016-12-02 18:35:41 +0800

    change to secret key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,7 @@
 Recaptcha.configure do |config|
   config.site_key  = Setting.recaptcha.public_key
   config.public_key  = Setting.recaptcha.public_key
+  config.secret_key = Setting.recaptcha.private_key
   config.private_key = Setting.recaptcha.private_key
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
